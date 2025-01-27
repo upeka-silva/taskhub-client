8e9762eda84bd8f6ba5b30ebaed04e80f711bfb9
upeka-silva <upekasilva2131@gmail.com>
2025-01-27 21:32:51 +0530

    initial changes

---
+++ taskhub-client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import RegistrationPage from "./components/RegistrationPage";
+
+function App() {
+  return (
+    <>
+      <RegistrationPage />
+    </>
+  );
+}
+
+export default App;
