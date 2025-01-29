3278e9a2988cfa26200159a468ef86256daa220b
upeka-silva <upekasilva2131@gmail.com>
2025-01-29 22:43:26 +0530

    api file changes

--- taskhub-client/src/api/api.js
+++ taskhub-client/src/api/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-export default  axios.create({
+export default axios.create({
   baseURL: "http://localhost:8080/",
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: "Bearer auth_token",
-  },
+  timeout: 10000,
 });
